9c28ed10e75a76748ad77be26fe1a99ac754f6a0
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-05-30 19:38:41 -0400

    added pre commit

---
+++ src/data_pull.py
@@ -0,0 +1,135 @@
+import logging
+from datetime import datetime
+from json import JSONDecodeError
+
+import polars as pl
+import requests
+
+from ..models import get_conn, init_dp03_table
+from .jp_qcew.src.data.data_process import cleanData
+
+
+class DataPull(cleanData):
+    def __init__(
+        self,
+        saving_dir: str = "data/",
+        database_file: str = "data.ddb",
+        log_file: str = "data_process.log",
+    ):
+        super().__init__(saving_dir, database_file, log_file)
+
+    def pull_query(self, params: list, year: int) -> pl.DataFrame:
+        # prepare custom census query
+        param = ",".join(params)
+        base = "https://api.census.gov/data/"
+        flow = "/acs/acs5/profile"
+        url = f"{base}{year}{flow}?get={param}&for=county%20subdivision:*&in=state:72&in=county:*"
+        df = pl.DataFrame(requests.get(url).json())
+
+        # get names from DataFrame
+        names = df.select(pl.col("column_0")).transpose()
+        names = names.to_dicts().pop()
+        names = dict((k, v.lower()) for k, v in names.items())
+
+        # Pivot table
+        df = df.drop("column_0").transpose()
+        return df.rename(names).with_columns(year=pl.lit(year))
+
+    def pull_dp03(self) -> pl.DataFrame:
+        if "DP03Table" not in self.conn.sql("SHOW TABLES;").df().get("name").tolist():
+            init_dp03_table(self.data_file)
+        for _year in range(2012, datetime.now().year):
+            if (
+                self.conn.sql(f"SELECT * FROM 'DP03Table' WHERE year={_year}")
+                .df()
+                .empty
+            ):
+                try:
+                    logging.info(f"pulling {_year} data")
+                    tmp = self.pull_query(
+                        params=[
+                            "DP03_0051E",
+                            "DP03_0052E",
+                            "DP03_0053E",
+                            "DP03_0054E",
+                            "DP03_0055E",
+                            "DP03_0056E",
+                            "DP03_0057E",
+                            "DP03_0058E",
+                            "DP03_0059E",
+                            "DP03_0060E",
+                            "DP03_0061E",
+                        ],
+                        year=_year,
+                    )
+                    tmp = tmp.rename(
+                        {
+                            "dp03_0051e": "total_house",
+                            "dp03_0052e": "inc_less_10k",
+                            "dp03_0053e": "inc_10k_15k",
+                            "dp03_0054e": "inc_15k_25k",
+                            "dp03_0055e": "inc_25k_35k",
+                            "dp03_0056e": "inc_35k_50k",
+                            "dp03_0057e": "inc_50k_75k",
+                            "dp03_0058e": "inc_75k_100k",
+                            "dp03_0059e": "inc_100k_150k",
+                            "dp03_0060e": "inc_150k_200k",
+                            "dp03_0061e": "inc_more_200k",
+                        }
+                    )
+                    tmp = tmp.with_columns(
+                        geoid=pl.col("state")
+                        + pl.col("county")
+                        + pl.col("county subdivision")
+                    ).drop(["state", "county", "county subdivision"])
+                    tmp = tmp.with_columns(pl.all().exclude("geoid").cast(pl.Int64))
+                    self.conn.sql("INSERT INTO 'DP03Table' BY NAME SELECT * FROM tmp")
+                    logging.info(f"succesfully inserting {_year}")
+                except JSONDecodeError:
+                    logging.warning(f"The ACS for {_year} is not availabe")
+                    continue
+            else:
+                logging.info(f"data for {_year} is in the database")
+                continue
+        return self.conn.sql("SELECT * FROM 'DP03Table';").pl()
+
+    def pull_file(url: str, filename: str, verify: bool = True) -> None:
+        """
+        Pulls a file from a URL and saves it in the filename. Used by the class to pull external files.
+
+        Parameters
+        ----------
+        url: str
+            The URL to pull the file from.
+        filename: str
+            The filename to save the file to.
+        verify: bool
+            If True, verifies the SSL certificate. If False, does not verify the SSL certificate.
+
+        Returns
+        -------
+        None
+        """
+        chunk_size = 10 * 1024 * 1024
+
+        with requests.get(url, stream=True, verify=verify) as response:
+            total_size = int(response.headers.get("content-length", 0))
+
+            with tqdm(
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+                desc="Downloading",
+            ) as bar:
+                with open(filename, "wb") as file:
+                    for chunk in response.iter_content(chunk_size=chunk_size):
+                        if chunk:
+                            file.write(chunk)
+                            bar.update(
+                                len(chunk)
+                            )
+
+    def pull_qcew(self,year:int,qrt:int,county:str) -> pl.DataFrame:
+    
+
