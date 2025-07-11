5e4c6e82b2c0bdce4c1ca48a0fec0fdef3571cdb
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-07-11 10:54:22 -0400

    impleme automize research

--- src/data_process.py
+++ src/data_process.py
@@ -3,6 +3,11 @@ import numpy as np
 import polars as pl
 import pandas as pd
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
+from toolz import partial
+from scipy.optimize import fmin_slsqp
+from joblib import Parallel, delayed
+import seaborn as sns
 
 
 class DataReg(DataPull):
@@ -39,7 +44,7 @@ class DataReg(DataPull):
         )
         return df
 
-    def controls_list(self) -> list:
+    def controls_list(self, target: str) -> list:
         df = self.data_set()
         df = df.filter((pl.col("industry_code") == "72") & (pl.col("year") == 2015))
         df_dp03 = self.pull_dp03()
@@ -61,27 +66,43 @@ class DataReg(DataPull):
         ).sort(by=["area_fips", "year"])
         selected_cols = ["commute_car", "employment", "total_population"]
 
+        data = data.with_columns(
+            area_fips="i" + pl.col("area_fips"),
+        )
+
         data_np = data.select(selected_cols).to_numpy()
 
-        # Compute the mean and covariance matrix
-        mean_vec = np.mean(data_np, axis=0)
-        cov_matrix = np.cov(data_np, rowvar=False)
+        # Separate the reference group (area_fips == '06081') and the target group (others)
+        reference_df = data.filter(pl.col("area_fips") == target)
+        target_df = data.filter(pl.col("area_fips") != target)
+
+        # Convert to numpy arrays
+        reference_np = reference_df.select(selected_cols).to_numpy()
+        target_np = target_df.select(selected_cols).to_numpy()
+
+        # Compute reference mean and inverse covariance matrix
+        mean_vec = np.mean(reference_np, axis=0)
+        cov_matrix = np.cov(target_np, rowvar=False)
         inv_cov_matrix = np.linalg.inv(cov_matrix)
 
-        # Compute Mahalanobis distance of each row from the mean
         mahalanobis_distances = [
             distance.mahalanobis(row, mean_vec, inv_cov_matrix) for row in data_np
         ]
 
-        # Add distances to the DataFrame
         data = data.with_columns(
-            mahalanobis=mahalanobis_distances, area_fips="i" + pl.col("area_fips")
+            mahalanobis=pl.Series("mahalanobis", mahalanobis_distances),
         )
-        controls = data.sort("mahalanobis").select("area_fips").to_series().to_list()
-
+        controls = (
+            data.filter(pl.col("area_fips") != target)
+            .sort("mahalanobis")
+            .head(200)
+            .select("area_fips")
+            .to_series()
+            .to_list()
+        )
         return controls
 
-    def synth_data(self, controls: list):
+    def synth_data(self, controls: list, target: str, date: str) -> pd.DataFrame:
         df = self.data_set()
         df = df.filter((pl.col("industry_code") == "72") & (pl.col("year") < 2020))
 
@@ -97,20 +118,247 @@ class DataReg(DataPull):
                 )
                 / 3
             ).log(),
-            # after_treatment=pl.when((pl.col("year") >= 2016) & (pl.col("qtr") > 1)).then(True).otherwise(False)
         )
         data = (
             df.select(pl.col("area_fips", "date", "total_employment", "avg_wkly_wage"))
             .with_columns(
-                controls=pl.when(pl.col("area_fips") == "i06081")
+                controls=pl.when(pl.col("area_fips") == target)
                 .then(True)
                 .otherwise(False)
             )
             .to_pandas()
         )
         data["date"] = pd.PeriodIndex(df["date"], freq="Q").to_timestamp()
-        data["after_treatment"] = data["date"] > pd.to_datetime("2016-01-01")
+        data["after_treatment"] = data["date"] > pd.to_datetime(date)
         data = data[
-            (data["area_fips"].isin(controls)) | (data["area_fips"] == "i06081")
+            (data["area_fips"].isin(controls)) | (data["area_fips"] == target)
         ].reset_index(drop=True)
-        return data
+        data = data[
+            (data["avg_wkly_wage"] != 0) & (data["avg_wkly_wage"].notnull())
+        ].reset_index(drop=True)
+        return pd.DataFrame(data)
+
+    def synth_freq(self, controls: list, target: str, date: str):
+        # Helper functions
+        def loss_w(W, X, y) -> float:
+            return np.sqrt(np.mean((y - X.dot(W)) ** 2))
+
+        def get_w(X, y):
+            w_start = [1 / X.shape[1]] * X.shape[1]
+
+            weights = fmin_slsqp(
+                partial(loss_w, X=X, y=y),
+                np.array(w_start),
+                f_eqcons=lambda x: np.sum(x) - 1,
+                bounds=[(0.0, 1.0)] * len(w_start),
+                disp=False,
+            )
+            return weights
+
+        def synthetic_control(area_fips: int, data: pd.DataFrame) -> np.array:
+            features = ["total_employment"]
+
+            inverted = (
+                data.query("~after_treatment")
+                .pivot(index="area_fips", columns="date")[features]
+                .T
+            )
+
+            y = inverted[area_fips].values  # treated
+            X = inverted.drop(columns=area_fips).values  # donor pool
+
+            weights = get_w(X, y)
+            synthetic = (
+                data.query(f'~(area_fips=="{area_fips}")')
+                .pivot(index="date", columns="area_fips")["total_employment"]
+                .values.dot(weights)
+            )
+
+            data = (
+                data.query(f'area_fips=="{area_fips}"')[
+                    ["area_fips", "date", "total_employment", "after_treatment"]
+                ].assign(synthetic=synthetic)
+            ).reset_index(drop=True)
+            return data
+
+        def pre_treatment_error(area_fips):
+            pre_treat_error = (
+                area_fips.query("~after_treatment")["total_employment"]
+                - area_fips.query("~after_treatment")["synthetic"]
+            ) ** 2
+            return pre_treat_error.mean()
+
+        data = self.synth_data(controls=controls, target=target, date=date)
+
+        # Remove invalide area fips
+        features = ["total_employment"]
+        pre_df = (
+            data.query("~after_treatment")
+            .pivot(index="area_fips", columns="date", values=features)
+            .T
+        ).dropna(axis=1)
+
+        post_df = (
+            data.query("after_treatment")
+            .pivot(index="area_fips", columns="date", values=features)
+            .T
+        )
+        pre_df = pre_df.dropna(axis=1)
+        post_df = post_df.dropna(axis=1)
+
+        controls = list(set(pre_df.columns) & set(post_df.columns))
+
+        data = data[data["area_fips"].isin(controls)].reset_index(drop=True)
+
+        features = ["total_employment"]
+
+        inverted = (
+            data.query("~after_treatment")
+            .pivot(index="area_fips", columns="date", values=features)
+            .T
+        )
+
+        y = inverted[target].values
+        X = inverted.drop(columns=target).values
+
+        synth_weights = get_w(X, y)
+        synth_control = (
+            data.query("~controls")
+            .pivot(index="date", columns="area_fips")["total_employment"]
+            .values.dot(synth_weights)
+        )
+        control_pool = data["area_fips"].unique()
+
+        parallel_fn = delayed(partial(synthetic_control, data=data))
+
+        synthetic_states = Parallel(n_jobs=8)(
+            parallel_fn(area_fips) for area_fips in control_pool
+        )
+
+        # Graph the Target and Donner pool
+        fig, ax = plt.subplots()
+
+        (
+            data.groupby(["date", "controls"], as_index=False)
+            .agg({"total_employment": "mean"})
+            .pipe(
+                (sns.lineplot, "data"),
+                x="date",
+                y="total_employment",
+                hue="controls",
+                marker="o",
+                ax=ax,
+            )
+        )
+        ax.axvline(
+            x=pd.to_datetime(date),
+            linestyle=":",
+            lw=2,
+            color="C2",
+            label="Implementation of minimum wage",
+        )
+
+        ax.legend(loc="upper right")
+        ax.set(title="Employment", ylabel="total employment trend Trend")
+
+        # Greph the synthetic control
+        plt.figure(figsize=(10, 6))
+        plt.plot(
+            data.query("controls")["date"],
+            data.query("controls")["total_employment"],
+            label="San Mateo",
+        )
+        plt.plot(
+            data.query("controls")["date"], synth_control, label="Synthetic Control"
+        )
+        plt.ylabel("Log employment")
+        plt.legend()
+
+        # Plot the impact
+        plt.figure(figsize=(10, 6))
+        plt.plot(
+            data.query("controls")["date"],
+            data.query("controls")["total_employment"] - synth_control,
+            label="San mateo Effect",
+        )
+        plt.hlines(
+            y=0,
+            xmin=pd.to_datetime("2014-01-01"),
+            xmax=pd.to_datetime("2020-01-01"),
+            lw=2,
+        )
+        plt.title("Counties - Synthetic Across Time")
+        plt.ylabel("Log of Employment")
+        plt.legend()
+
+        # Greph lines
+        plt.figure(figsize=(12, 7))
+        for area_fips in synthetic_states:
+            plt.plot(
+                area_fips["date"],
+                area_fips["total_employment"] - area_fips["synthetic"],
+                color="C5",
+                alpha=0.4,
+            )
+
+        plt.plot(
+            data.query("controls")["date"],
+            data.query("controls")["total_employment"] - synth_control,
+            label="San mateo",
+        )
+
+        plt.vlines(
+            x=pd.to_datetime(date),
+            ymin=-0.25,
+            ymax=0.25,
+            linestyle=":",
+            lw=2,
+            label="polosy",
+        )
+        plt.hlines(
+            y=0,
+            xmin=pd.to_datetime("2014-01-01"),
+            xmax=pd.to_datetime("2020-01-01"),
+            lw=3,
+        )
+        plt.ylabel("Gap in log employment")
+        plt.title("Counties - Synthetic Across Time")
+        plt.legend()
+
+        # Grph without outliers
+        plt.figure(figsize=(12, 7))
+        for area_fips in synthetic_states:
+            if pre_treatment_error(area_fips) < 80:
+                plt.plot(
+                    area_fips["date"],
+                    area_fips["total_employment"] - area_fips["synthetic"],
+                    color="C5",
+                    alpha=0.4,
+                )
+
+        plt.plot(
+            data.query("controls")["date"],
+            data.query("controls")["total_employment"] - synth_control,
+            label="California",
+        )
+
+        plt.vlines(
+            x=pd.to_datetime(date),
+            ymin=-0.25,
+            ymax=0.25,
+            linestyle=":",
+            lw=2,
+            label="polosy",
+        )
+        plt.hlines(
+            y=0,
+            xmin=pd.to_datetime("2014-01-01"),
+            xmax=pd.to_datetime("2020-01-01"),
+            lw=3,
+        )
+        plt.ylabel("Gap in log Employment")
+        plt.title("Distribution of Effects")
+        plt.title(
+            "counties - Synthetic Across Time (Large Pre-Treatment Errors Removed)"
+        )
+        plt.legend()
